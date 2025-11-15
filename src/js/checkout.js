6aff307cf2bda926f7bc43fa9cec404c3000a770
gianvin <ginamangahassanchezong@gmail.com>
2025-11-15 20:11:19 +0800

    write the function renderCheckoutTotal at checkout.js

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,18 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter, applyDiscount } from "./utils.mjs";
 
 loadHeaderFooter();
+
+function renderCheckoutTotal() {
+    const cartItems = getLocalStorage("so-cart") || [];
+    const totalContainer = document.querySelector(".checkout-total")
+
+    if (!totalContainer) return;
+
+    const total = cartItems.reduce((sum, item) => {
+        const discountedPrice = item.Discount ? applyDiscount(item.Price, item.Discount) : item.Price;
+        return sum + discountedPrice;
+    }, 0);
+
+    totalContainer.textContent = `Total: $${total.toFixed(2)}`;
+}
+renderCheckoutTotal();
